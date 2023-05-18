e9193f75afac734cc1617fcd78ab1a986148bcde
ljpoli <ljpolicano@gmail.com>
2023-05-18 14:03:59 -0400

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header/Header";
-
+import NavBar from "./components/NavBar/NavBar";
 class App extends React.Component {
   render(){    
   return (
     <div>
+      <NavBar />
       <Header title= "Tienda de Arte" />
+   
     </div>
+ 
   );
 }
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,22 @@
-g
+import React from "react";
+import "./navbar.css"
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <ul>
+            
+          <li>
+            <a href="www.inicio.com">Inicio</a>
+         </li>
+          <li>
+            <a href="www.acercade.com">Acerca de</a>
+         </li>
+          <li>
+                <a href="www.contacto.com">Contacto</a>
+         </li>
+        </ul>
+    </nav>
+  )
+}
+export default NavBar;
